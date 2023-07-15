ce0eaf338468a6a8932df291dd97fac040ecd6bc
Abul <abul.khayar@hotmail.com>
2023-07-15 13:38:42 +0100

    test v2 - Button

--- app/src/main/java/com/example/characterencyclopedia/MainActivity.java
+++ app/src/main/java/com/example/characterencyclopedia/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.characterencyclopedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button1);
+
+        button.setOnClickListener(View  ->{
+            Toast.makeText(View.getContext(), "test", Toast.LENGTH_SHORT).show();
+
+        });
     }
 }
